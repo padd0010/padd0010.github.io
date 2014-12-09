c60d3d24ee53c8f2a986964b182e6f34aa8da5c6
padd0010 <padd0010@algonquinlive.com>
2014-12-09 12:26:18 -0500

    404 Page
    Added styles and image to 404 page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ $win.on('scroll', function () {
 
 	$homeSection.css('background-position', 'center ' + scrollPos / 3 + 'px');	
 });
+
+function hover(element) {
+    element.setAttribute('src', 'images/happy-cloud.png');
+}
+function unhover(element) {
+    element.setAttribute('src', 'images/sad-cloud.png');
+}
